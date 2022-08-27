4a1ccb9866fcad0f6d6a51784ed18f31145e2cbe
Aklu <201098@kit.ac.in>
2022-08-27 11:20:51 +0530

    drawin app 2.0

---
+++ script.js
@@ -0,0 +1,46 @@
+
+function startPainting(e)
+{
+	painting=true;
+	draw(e);
+}
+
+function stopPainting()
+{
+	painting=false;
+	ctx.beginPath();
+}
+
+function draw(e)
+{
+	if(!painting) return
+
+	ctx.lineWidth=10;
+	ctx.lineCap='round';
+	ctx.lineTo(e.clientX,e.clientY);
+	ctx.stroke();
+	ctx.beginPath();
+	ctx.moveTo(e.clientX,e.clientY);
+}
+	let canvas=document.getElementById("canva");
+
+	let ctx=canvas.getContext("2d");
+	let painting=false;
+
+
+	canvas.width=window.innerWidth;
+	canvas.height=window.innerHeight;
+
+	canvas.addEventListener("touchstart",startPainting);
+	canvas.addEventListener("mousedown",startPainting);
+
+	canvas.addEventListener("mouseup",stopPainting);
+	canvas.addEventListener("touchend",stopPainting);
+	
+	canvas.addEventListener("mousemove",draw);
+	canvas.addEventListener("touchmove",draw);
+
+document.getElementById("btn").onclick=(e)=>
+{
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+}
